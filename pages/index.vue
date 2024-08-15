f826fcbca0b29244aa8f4e4ac2c3540641e305dc
irfanazizalamin <irfanaziz2929@gmail.com>
2024-08-15 16:33:24 +0700

    feat: add sort dropdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,38 @@
 <template>
-  <NuxtLayout name="main-layout">
-    <div class="h-screen relative isolate overflow-hidden py-32 px-10">
-      <img
-        src="https://images.unsplash.com/photo-1652410057727-f0424a870692?q=80&w=2831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-        class="absolute inset-0 -z-10 h-full w-full object-cover"
-      />
+  <div class="h-screen relative isolate overflow-hidden py-32 px-10">
+    <img
+      src="https://images.unsplash.com/photo-1652410057727-f0424a870692?q=80&w=2831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt=""
+      class="absolute inset-0 -z-10 h-full w-full object-cover"
+    />
 
-      <div
-        class="bg-white/50 backdrop-blur-lg mx-auto max-w-4xl h-full flex items-center px-10 rounded-lg shadow"
-      >
-        <div class="text-center w-full">
-          <h1
-            class="text-4xl font-semibold tracking-tight sm:text-6xl text-white"
-          >
-            <div class="font-bold text-white">"ONLY TODAY"</div>
-            <p class="mt-4 text-2xl">you will get Special Price only in:</p>
-          </h1>
-          <ClientOnly>
-            <div class="flex items-center justify-center">
-              <div class="mt-5 text-white py-2 px-4 w-fit">
-                <h1 class="text-4xl font-bold">{{ formattedTime }}</h1>
-              </div>
+    <div
+      class="bg-white/50 backdrop-blur-lg mx-auto max-w-4xl h-full flex items-center px-10 rounded-lg shadow"
+    >
+      <div class="text-center w-full">
+        <h1
+          class="text-4xl font-semibold tracking-tight sm:text-6xl text-white"
+        >
+          <div class="font-bold text-white">"ONLY TODAY"</div>
+          <p class="mt-4 text-2xl">you will get Special Price only in:</p>
+        </h1>
+        <ClientOnly>
+          <div class="flex items-center justify-center">
+            <div class="mt-5 text-white py-2 px-4 w-fit">
+              <h1 class="text-4xl font-bold">{{ formattedTime }}</h1>
             </div>
-          </ClientOnly>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <NuxtLink
-              to="/products"
-              class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primaryLighter focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-              >Lets Check Our Products</NuxtLink
-            >
           </div>
+        </ClientOnly>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <NuxtLink
+            to="/products"
+            class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primaryLighter focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+            >Lets Check Our Products</NuxtLink
+          >
         </div>
       </div>
     </div>
-  </NuxtLayout>
+  </div>
 </template>
 
 <script setup>
@@ -80,5 +78,4 @@ onUnmounted(() => {
 });
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,7 +1,58 @@
 <template>
   <div class="container mx-auto py-10 lg:py-14 px-5 lg:px-0">
-    <div class="bg-primary mb-5 p-2 rounded shadow-sm">
-      <h2 class="font-medium text-xl text-white text-center">Products List</h2>
+    <div class="flex justify-between">
+      <div>
+        <div class="bg-primary mb-5 p-2 rounded shadow-sm">
+          <h2 class="font-medium text-xl text-white text-center">
+            Products List
+          </h2>
+        </div>
+      </div>
+      <div class="relative">
+        <button
+          type="button"
+          class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          @click="isSortDropdownOpen = !isSortDropdownOpen"
+        >
+          Sort By
+          <svg
+            class="-mr-1 h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+
+        <div
+          v-if="isSortDropdownOpen"
+          class="absolute right-0 z-[99] mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabindex="-1"
+        >
+          <div class="py-1" role="none">
+            <a
+              v-for="(option, index) in sortOptions"
+              :key="index"
+              href="#"
+              class="block px-4 py-2 text-sm text-gray-700"
+              :class="{
+                'bg-primary text-gray-900': selectedSortValue === option.value,
+              }"
+              @click="onSortProduct(option.value)"
+            >
+              {{ option.name }}
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
     <ProductList :is-loading="isLoading" :products="products" />
   </div>
@@ -19,15 +70,37 @@ const { fetchAllProducts } = useProduct();
 
 const isLoading = ref(true);
 const products = ref([]);
+const sortedProducts = ref([]);
 async function getAllProducts() {
   try {
-    products.value = await fetchAllProducts();
+    const response = await fetchAllProducts();
+    products.value = response;
+    sortedProducts.value = response;
   } catch (error) {
     console.error(error);
   } finally {
     isLoading.value = false;
   }
 }
+
+const isSortDropdownOpen = ref(false);
+const selectedSortValue = ref("");
+const sortOptions = [
+  { name: "Price: Low to High", value: "price-asc" },
+  { name: "Price: High to Low", value: "price-desc" },
+];
+
+function onSortProduct(value) {
+  selectedSortValue.value = value;
+  isSortDropdownOpen.value = false;
+
+  if (value === "price-asc") {
+    sortedProducts.value = products.value.sort((a, b) => a.price - b.price);
+  } else if (value === "price-desc") {
+    sortedProducts.value = products.value.sort((a, b) => b.price - a.price);
+  }
+}
+
 
 onMounted(() => {
   getAllProducts();
